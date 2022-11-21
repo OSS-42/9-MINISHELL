45c26e121d93f623a29d3bc392e6bf02ecab17c7
Mbertin44 <morganbertin44@gmail.com>
2022-11-21 16:05:27 -0500

    clean du minishell.h

--- src/explore_readline.c
+++ src/explore_readline.c
@@ -6,7 +6,7 @@
 /*   By: momo <momo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 13:55:29 by momo              #+#    #+#             */
-/*   Updated: 2022/11/21 15:45:07 by momo             ###   ########.fr       */
+/*   Updated: 2022/11/21 15:50:34 by momo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	explore_readline(t_data	*data)
 	data->read_line = readline("alive>");
 	add_history(data->read_line);
 	data->split_readline = ft_split(data->read_line, ' ');
-	if (ft_strcmp("cd", data->split_readline[0]) == 1)
+	if (ft_strncmp("cd", data->split_readline[0], ft_strlen(data->split_readline[0])) == 0)
 		ft_cd(data);
 	// if (ft_strncmp(data->split_readline[0], "pwd", 3) == 0)
 	// 	if (getcwd())
