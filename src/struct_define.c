98887defcc8d0c60832dfef3315721f4da33b976
Mbertin44 <morganbertin44@gmail.com>
2022-11-23 10:52:15 -0500

    clean de la fonction init_struct dans struct_define.c

--- src/struct_define.c
+++ src/struct_define.c
@@ -6,26 +6,13 @@
 /*   By: mbertin <mbertin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 12:19:32 by momo              #+#    #+#             */
-/*   Updated: 2022/11/22 09:34:06 by mbertin          ###   ########.fr       */
+/*   Updated: 2022/11/23 10:51:39 by mbertin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-extern char	**environ;
-
-// void	malloc_struct(t_vault **data, int argc, char **argv, char **env)
+// void	init_struct(t_vault **data)
 // {
-// 	*data = malloc(sizeof (t_vault));
-// 	if (!*data)
-// 	{
-// 		free(data);
-// 		exit(EXIT_FAILURE);
-// 	}
-// 	init_struct(*data, argc, argv, env);
+// 	(*data)->env = environ;
 // }
-
-void	init_struct(t_vault **data)
-{
-	(*data)->env = environ;
-}
