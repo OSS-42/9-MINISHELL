5f16ab3347c775007aa72a47bc65ac82fddb6baf
xDeadpoolx <eric.wursteisen@gmail.com>
2023-01-12 16:05:59 -0500

    norm fixing 2

--- src/minishell.c
+++ src/minishell.c
@@ -6,71 +6,13 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 15:22:01 by ewurstei          #+#    #+#             */
-/*   Updated: 2023/01/12 16:02:42 by ewurstei         ###   ########.fr       */
+/*   Updated: 2023/01/12 16:05:43 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
 int	g_error_code = 0;
-
-void	init_data(t_vault *data, char **env)
-{
-	put_code_in_fd("0\0", data->error_fd);
-	// data->error_fd = open("/tmp/.tmp_error",
-	// 		O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	// ft_putstr_fd("0\0", data->error_fd);
-	// close(data->error_fd);
-	data->env = ft_dbl_ptr_copy(env);
-	data->buffer = NULL;
-	data->cmd = ft_calloc(sizeof(t_cmd), 1);
-	data->b_in = ft_calloc(sizeof(t_builtins), 1);
-	data->quote = ft_calloc(sizeof(t_quote), 1);
-	data->flag = ft_calloc(sizeof(t_flag), 1);
-	data->rl_dec = NULL;
-	data->dollar_var = NULL;
-	data->read_line = NULL;
-	data->tab_arg = NULL;
-	data->dollar_var_len = 0;
-	data->path_names = NULL;
-	data->flag->stdout_backup = dup(STDOUT_FILENO);
-	data->flag->stdin_backup = dup(STDIN_FILENO);
-	data->fail_redir = FALSE;
-	data->flag->execve = 0;
-	data->flag->rl_exit = 0;
-	data->pid = NULL;
-	return ;
-}
-
-void	reinit_data(t_vault *data)
-{
-	data->flag->pipe_count = 0;
-	if (data->flag->fd_out > 0)
-		close (data->flag->fd_out);
-	if (data->flag->fd > 0)
-		close (data->flag->fd);
-	if (data->flag->heredoc_fd > 0)
-	{
-		unlink("temp_heredoc");
-		close (data->flag->heredoc_fd);
-	}
-	data->flag->heredoc_fd = 0;
-	data->flag->fd_out = 0;
-	data->flag->fd = 0;
-	data->quote->double_quote_count = 0;
-	data->quote->simple_quote_count = 0;
-	data->quote->begin = 0;
-	data->quote->last_replace = 0;
-	data->fail_redir = FALSE;
-	ft_free_n_null (data->cmd->name);
-	data->cmd->name = NULL;
-	if (data->cmd->opt)
-		ft_dbl_ptr_free((void **)data->cmd->opt);
-	data->cmd->opt = NULL;
-	if (data->tab_arg)
-		ft_dbl_ptr_free((void **)data->tab_arg);
-	data->tab_arg = NULL;
-}
 
 void	readline_exec(t_vault *data)
 {
