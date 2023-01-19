64b2157ebbcbceaf3fff80d65b2431506a04e7ba
Mbertin44 <morganbertin44@gmail.com>
2023-01-19 12:10:48 -0500

    Correction du waitpid entre chaque fork

--- src/explore_readline.c
+++ src/explore_readline.c
@@ -6,7 +6,7 @@
 /*   By: momo <momo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 13:55:29 by momo              #+#    #+#             */
-/*   Updated: 2023/01/19 11:40:07 by momo             ###   ########.fr       */
+/*   Updated: 2023/01/19 12:07:54 by momo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,6 +109,8 @@ void	forking(t_vault *data, int line, int type)
 	}
 	else if (type == 2)
 	{
+		if (line > 0)
+			waitpid(data->pid[line - 1], &data->status, 0); // A voir avec eric
 		child_creation(data, line);
 		if (data->pid[line] == 0)
 		{
@@ -119,7 +121,6 @@ void	forking(t_vault *data, int line, int type)
 			close_pipe(data);
 			exit_process(data);
 		}
-		waitpid(data->pid[line], &data->status, 0); // A voir avec eric
 	}
 }
 
--- src/heredoc.c
+++ src/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: momo <momo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 15:24:04 by mbertin           #+#    #+#             */
-/*   Updated: 2023/01/19 11:36:13 by momo             ###   ########.fr       */
+/*   Updated: 2023/01/19 12:01:39 by momo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,8 @@ void	heredoc(t_vault *data, int line)
 		check_eof(data, str);
 	}
 	reset_n_close_heredoc(data);
-	if (line == 0 || line != ft_dbl_ptr_len(data->tab_arg) - 1)
+	if (data->flag->pipe_count > 0
+		&& (line == 0 || line != ft_dbl_ptr_len(data->tab_arg) - 1))
 		dup2(data->flag->pipe[line][p_write], STDOUT_FILENO);
 	if (data->flag->fd_out > 0)
 		dup2(data->flag->fd_out, STDOUT_FILENO);
